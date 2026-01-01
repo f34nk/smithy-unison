08bd0638d1635f2a2ea8a4d87af93b1e388e697e
Frank Eickhoff <info@frankeickhoff.de>
2026-01-01 21:26:11 +0100

    chore: simplify dynamodb-demo terraform for LocalStack compatibility
    Remove replica and stream configuration from DynamoDB table definition.
    LocalStack does not support multi-region replicas, causing terraform apply to fail.

--- examples/dynamodb-demo/terraform/main.tf
+++ examples/dynamodb-demo/terraform/main.tf
@@ -22,22 +22,12 @@ provider "aws" {
 }
 
 resource "aws_dynamodb_table" "example" {
-  name             = "example"
-  hash_key         = "TestTableHashKey"
-  billing_mode     = "PAY_PER_REQUEST"
-  stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
+  name         = "example"
+  hash_key     = "TestTableHashKey"
+  billing_mode = "PAY_PER_REQUEST"
 
   attribute {
     name = "TestTableHashKey"
     type = "S"
-  }
-
-  replica {
-    region_name = "us-east-1"
-  }
-
-  replica {
-    region_name = "us-west-1"
   }
 }
